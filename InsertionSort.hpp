15f2ba26ac96cde0a58e91a59553ba22f333a091
Maxim <maxim.kr7375@gmail.com>
2018-10-06 21:49:18 +0300

    some fixed

--- InsertionSort.hpp
+++ InsertionSort.hpp
@@ -1,32 +1,16 @@
-#include <vector>
 using namespace std;
-
     template<class T>
-    vector <T> sort(vector<T> const &input)
-    {
-        vector <T> output(input);
-        vector <T>  key(1);
-        if (input.size()<2)
-        {
-            return output;
-        }
-        for (int j=1;j<output.size();j++)
-        {
-            key[0]=output[j];
-            int i=j-1;
-            while (i>=0 && output[i]>key[0])
+       vector <T> sort(vector<T> const &input)
+       {
+            vector <T> output=input;
+            for (int j=1;j<output.size();j++)
             {
-                output[i+1]=output[i];
-                if (i>0)
+              int i=j;
+              while (i>0 && output[i-1]>output[i])
                 {
-                    i=i-1;
-                    output[i+1]=key[0];
-                }
-                else
-                {
-                    output[i]=key[0];
+                  swap(output[i],output[i-1]);
+                  i--;
                 }
             }
-        }
-        return output;
-    };
+          return output;
+      };
